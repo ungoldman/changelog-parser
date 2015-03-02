9bfec66a44adb37789b64082c9cb6b54d4aa6f27
Nate Goldman <nnmgoldman@gmail.com>
2015-03-01 17:09:06 -0800

    switch to parsing html

--- index.js
+++ index.js
@@ -1,28 +1,43 @@
-module.exports = function parse (file) {
-  var changelog = {}
-  var versions = getVersions(file)
+var marked = require('marked')
+var toMarkdown = require('to-markdown').toMarkdown
+var cheerio = require('cheerio')
+var regex = /\[?([\w\d\.-]+\.[\w\d\.-]+[a-zA-Z0-9])\]? .*?(\d{4}-\d{2}-\d{2}|\w+)/
+var $
 
-  versions.forEach(function (v) {
-    changelog[v] = getBody(v)
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: true,
+  smartLists: true
+})
+
+module.exports = function parse (file) {
+  $ = cheerio.load(marked(file))
+
+  return {
+    title: getTitle(),
+    versions: getVersions()
+  }
+}
+
+function getTitle () {
+  return $('h1').text()
+}
+
+function getVersions () {
+  var arr = []
+
+  var versions = $('h2').filter(function () {
+    return regex.test($(this).text())
   })
 
-  return changelog
-}
+  versions.each(function () {
+    arr.push({
+      version: $(this).text(),
+      body: toMarkdown($(this).nextUntil('h2').html())
+    })
+  })
 
-function getVersions (string) {
-  var matches = []
-  var regex = /#{1,3} ?\[?v?([\w\d\.-]+\.[\w\d\.-]+[a-zA-Z0-9])\]?/gi
-
-  var match = regex.exec(string)
-
-  while (match) {
-    matches.push(match[1])
-    match = regex.exec(string)
-  }
-
-  return matches
-}
-
-function getBody (version) {
-  return 'asdasdas'
+  return arr
 }
--- test/CHANGELOG.md
+++ test/CHANGELOG.md
@@ -1,14 +1,15 @@
 # test changelog
 
-## unreleased
-
-## [X.Y.Z-pre.aplhaa.asdf.asdf] - Unreleased
+## [3.0.0] - Unreleased
 * Update API
 * Fix bug #1
 
-## 1.2.3-pre.1 / 2013-02-14
+## 2.2.3-pre.1 - 2013-02-14
 * Update API
 
-## 1.0.0-x.7.z.92 / 2013-02-14
+## 1.0.0-x.7.z.92 - 2013-02-14
+* bark bark
+* woof
+* arf
 
-[X.Y.Z-pre.aplhaa.asdf.asdf][a cool link]
+[3.0.0]: http://npmpjs.com
